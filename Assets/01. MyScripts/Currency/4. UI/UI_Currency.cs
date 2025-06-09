fdcb5055ad4692e21a8382d765c61f449ada5731
nam3856 <nam3856@gmail.com>
2025-06-09 14:05:38 +0900

    feature: Added UI

---
+++ Assets/01. MyScripts/Currency/4. UI/UI_Currency.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class UI_Currency : MonoBehaviour
+{
+    public TextMeshProUGUI GoldCountText;
+    public TextMeshProUGUI DiamondCountText;
+
+    private void Start()
+    {
+        // 초기화
+        Refresh();
+        // 이벤트 구독
+        CurrencyManager.Instance.CurrenciesChanged += Refresh;
+    }
+
+    private void OnDestroy()
+    {
+        // 이벤트 구독 해제
+        if (CurrencyManager.Instance != null)
+        {
+            CurrencyManager.Instance.CurrenciesChanged -= Refresh;
+        }
+    }
+    private void Refresh()
+    {
+        var dtoList = CurrencyManager.Instance.GetAllCurrencyDTOs();
+        foreach (var dto in dtoList)
+        {
+            switch (dto.CurrencyType)
+            {
+                case ECurrencyType.Gold:
+                    GoldCountText.text = dto.CurrencyType.ToString() + dto.Value.ToString();
+                    break;
+                case ECurrencyType.Diamond:
+                    DiamondCountText.text = dto.CurrencyType.ToString() + dto.Value.ToString();
+                    break;
+            }
+        }
+    }
+}
