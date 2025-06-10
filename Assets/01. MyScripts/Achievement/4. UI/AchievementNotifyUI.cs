6657d3bd651f8a92ba67a16e409261e832444028
nam3856 <nam3856@gmail.com>
2025-06-10 18:06:39 +0900

    feature: Achievement Notification Queue
    업적 한번에 안나오게 수정

--- Assets/01. MyScripts/Achievement/4. UI/AchievementNotifyUI.cs
+++ Assets/01. MyScripts/Achievement/4. UI/AchievementNotifyUI.cs
@@ -1,23 +1,50 @@
 ﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class AchievementNotifyUI : MonoBehaviour
 {
     [SerializeField] private GameObject _popupPrefab;
     [SerializeField] private Transform _popupParent;
+    [SerializeField] private float popupDuration = 2f; // 한 팝업이 유지되는 시간
+
+    private Queue<AchievementDTO> _popupQueue = new Queue<AchievementDTO>();
+    private bool _isShowing = false;
 
     private void Start()
     {
-        GameManager.Instance.Events.Achievement.OnAchievementUnlocked += ShowPopup;
+        GameManager.Instance.Events.Achievement.OnAchievementUnlocked += EnqueuePopup;
     }
 
     private void OnDestroy()
     {
-        GameManager.Instance.Events.Achievement.OnAchievementUnlocked -= ShowPopup;
+        GameManager.Instance.Events.Achievement.OnAchievementUnlocked -= EnqueuePopup;
     }
 
-    private void ShowPopup(AchievementDTO dto)
+    private void EnqueuePopup(AchievementDTO dto)
     {
-        GameObject go = Instantiate(_popupPrefab, _popupParent);
-        go.GetComponent<AchievementPopup>().SetData(dto);
+        _popupQueue.Enqueue(dto);
+        if (!_isShowing)
+        {
+            StartCoroutine(ProcessQueue());
+        }
+    }
+
+    private IEnumerator ProcessQueue()
+    {
+        _isShowing = true;
+
+        while (_popupQueue.Count > 0)
+        {
+            var dto = _popupQueue.Dequeue();
+            GameObject go = Instantiate(_popupPrefab, _popupParent);
+            go.GetComponent<AchievementPopup>().SetData(dto);
+
+            yield return new WaitForSecondsRealtime(popupDuration);
+
+            Destroy(go);
+        }
+
+        _isShowing = false;
     }
 }
--- Assets/01. MyScripts/Achievement/4. UI/AchievementPopup.cs
+++ Assets/01. MyScripts/Achievement/4. UI/AchievementPopup.cs
@@ -4,17 +4,10 @@ using System.Collections;
 public class AchievementPopup : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
-    [SerializeField] private float _duration = 3f;
 
     public void SetData(AchievementDTO dto)
     {
         _text.text = $"[업적 달성] {dto.Name}";
-        StartCoroutine(AutoClose());
     }
 
-    private IEnumerator AutoClose()
-    {
-        yield return new WaitForSeconds(_duration);
-        Destroy(gameObject);
-    }
 }
