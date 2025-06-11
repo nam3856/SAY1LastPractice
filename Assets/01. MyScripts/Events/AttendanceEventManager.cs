44ff7bcdf0e2b5c14930015a1621f6ee73907ed3
nam3856 <nam3856@gmail.com>
2025-06-11 16:31:55 +0900

    feature: Attendance Event 추가

---
+++ Assets/01. MyScripts/Events/AttendanceEventManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class AttendanceEventManager
+{
+    public event Action OnAttendanceInitialized;
+    public event Action OnTodayAttendanceChecked;
+    public event Action<ECurrencyType, int> OnAttendanceRewardClaimed;
+    public void RaiseAttendanceInitialized()
+    {
+        OnAttendanceInitialized?.Invoke();
+    }
+    public void RaiseTodayAttendanceChecked()
+    {
+        OnTodayAttendanceChecked?.Invoke();
+    }
+    public void RaiseRewardClaimed(AttendanceSlotDTO attendanceSlotDTO)
+    {
+
+        OnAttendanceRewardClaimed?.Invoke(attendanceSlotDTO.RewardCurrencyType, attendanceSlotDTO.RewardCurrencyAmount);
+    }
+
+}
--- Assets/01. MyScripts/Events/GameEventSystem.cs
+++ Assets/01. MyScripts/Events/GameEventSystem.cs
@@ -4,4 +4,6 @@ public class GameEventSystem
 {
     public CurrencyEventManager Currency { get; } = new CurrencyEventManager();
     public AchievementEventManager Achievement { get; } = new AchievementEventManager();
+
+    public AttendanceEventManager Attendance { get; } = new AttendanceEventManager();
 }
