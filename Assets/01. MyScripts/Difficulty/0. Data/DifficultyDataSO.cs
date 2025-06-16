d929c10a1754dacd3fc4ffab8546cef9e8b37d50
nam3856 <nam3856@gmail.com>
2025-06-16 13:09:24 +0900

    feature: Difficulty 초기세팅

---
+++ Assets/01. MyScripts/Difficulty/0. Data/DifficultyDataSO.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "DifficultyData", menuName = "Difficulty/Create New DifficultyData")]
+
+public class DifficultyDataSO : ScriptableObject
+{
+    [Header("난이도 종류")]
+    public EDifficulty DifficultyType;
+    [Header("난이도 이름")]
+    public string Name;
+    [Header("난이도 레벨")]
+    public int Level;
+    [Header("적 체력 비율")]
+    public float EnemyHealthMultiplier = 1.0f;
+    [Header("적 공격력 비율")]
+    public float EnemyDamageMultiplier = 1.0f;
+    [Header("엘리트 적 스폰 빈도")]
+    public float EliteSpawnRate = 0.1f;
+    [Header("보스 적 스폰 빈도")]
+    public float BossSpawnRate = 0.05f;
+}
---
+++ Assets/01. MyScripts/Difficulty/0. Data/EDifficulty.cs
@@ -0,0 +1,10 @@
+﻿public enum EDifficulty
+{
+    VeryEasy = 0,
+    Easy = 1,
+    Normal = 2,
+    Hard = 3,
+    VeryHard = 4,
+    Nightmare = 5,
+    Insane = 6
+}
