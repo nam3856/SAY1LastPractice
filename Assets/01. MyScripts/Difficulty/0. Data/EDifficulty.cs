79de9692839ff8c9cef02dde8ddc6766fea773ce
nam3856 <nam3856@gmail.com>
2025-06-16 14:48:41 +0900

    Update EDifficulty.cs

--- Assets/01. MyScripts/Difficulty/0. Data/EDifficulty.cs
+++ Assets/01. MyScripts/Difficulty/0. Data/EDifficulty.cs
@@ -1,10 +1,12 @@
 ﻿public enum EDifficulty
 {
-    VeryEasy = 0,
-    Easy = 1,
-    Normal = 2,
-    Hard = 3,
-    VeryHard = 4,
-    Nightmare = 5,
-    Insane = 6
+    Easy = 0,
+    Normal = 1,
+    Hard = 2,
+    VeryHard = 3,
+    Insane = 4,
+    Impossible = 5,
+    ISeeYou = 6,
+    ImCommingForYou = 7,
+    Hahahaha = 8
 }
