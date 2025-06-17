b44fd5baa519a5eeb9a99ad6c53f39c456c1897e
김나현 <skgus917@gmail.com>
2025-06-17 17:07:04 +0900

    [KNH] 랭킹 UI 스크립트 추가

--- Assets/01. MyScripts/Score&Ranking/Ranking/4. UI/UI_Ranking.cs
+++ Assets/01. MyScripts/Score&Ranking/Ranking/4. UI/UI_Ranking.cs
@@ -1,16 +1,43 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class UI_Ranking : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private GameObject rankingPrefab;
+    [SerializeField] private Transform entryContainer;
+    [SerializeField] private List<UI_RankingEntry> entryUIList = new List<UI_RankingEntry>();
+    [SerializeField] private UI_RankingEntry myScoreUI;
+
+    private void Start()
     {
-        
+        InitEmptyRanking();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitEmptyRanking()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            var entryGO = Instantiate(rankingPrefab, entryContainer);
+            var entryUI = entryGO.GetComponent<UI_RankingEntry>();
+            entryUI.SetData("-", "-", "-");
+            entryUIList.Add(entryUI);
+        }
+    }
+
+    public void UpdateRanking(List<RankingDataSO> rankingList, RankingDataSO myData)
+    {
+        for (int i = 0; i < entryUIList.Count; i++)
+        {
+            if (i < rankingList.Count)
+            {
+                var data = rankingList[i];
+                entryUIList[i].SetData(data.Rank.ToString(), data.PlayerName, data.Score.ToString());
+            }
+            else
+            {
+                entryUIList[i].SetData("-", "-", "-");
+            }
+        }
+        myScoreUI.SetData("-", myData.PlayerName, myData.Score.ToString());
     }
 }
